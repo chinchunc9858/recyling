6604ca403e359499c5e3a14477817c8aea9d2ad0
Ali Chen <alichen247@gmail.com>
2025-02-22 21:16:40 -0500

    lol

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+
+
 import logo from './logo.svg';
 import './App.css';
+
+import Frame from './components/Frame';
+import Camera from './components/Camera';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <p>losdl</p>
+
+        <Frame />
+
       </header>
     </div>
   );
